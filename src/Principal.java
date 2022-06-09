1bf2f3dfec1267c6f6c1b7b187b7533942058d30
Guido Perre <perreguido@gmail.com>
2022-06-09 12:05:38 -0300

    check

--- src/Principal.java
+++ src/Principal.java
@@ -3,51 +3,83 @@ import java.util.ArrayList;
 public class Principal {
 
     public static void main(String[] args) {
+        imprimirResultados("Ejemplo 1", ejemplo1());
+        imprimirResultados("Ejemplo 2", ejemplo2());
+    }
+
+    public static ArrayList<Resultado> ejemplo1() {
         ResolverCandidatosImplementacion encontrarCandidatos = new ResolverCandidatosImplementacion();
-        ArrayList<Vacante> vacantes = crearVacantes();
-        ArrayList<Candidato> candidatos = crearCandidatos();
+        ArrayList<Vacante> vacantes = crearVacantes1();
+        ArrayList<Candidato> candidatos = crearCandidatos1();
         int maxCombinaciones = 4;
 
-        ArrayList<Resultado> resultados = encontrarCandidatos.obtenerCandidatos(vacantes, candidatos, maxCombinaciones);
-
-        // USAR ESTE PARA VER TODOS LOS RESULTADOS VALIDOS
-        imprimirResultados(resultados);
-
-        // USAR ESTE PARA VER RESULTADOS VALIDOS SIN REPETIDOS
-        //imprimirResultadosSinRepetidos(resultados);
+        return encontrarCandidatos.obtenerCandidatos(vacantes, candidatos, maxCombinaciones);
     }
 
-    private static void imprimirResultados(ArrayList<Resultado> resultados) {
-        for (Resultado resultado : resultados) {
-            System.out.println("Resultado " + resultado.idResultado);
-
-            for (PuestoCubierto puesto : resultado.puestosCubiertos) {
-                System.out.print("[" + puesto.nombreVacante + "]");
-            }
-            System.out.println();
-            for (PuestoCubierto puesto : resultado.puestosCubiertos) {
-                System.out.print("[" + puesto.idCandidato + "]");
-            }
-            System.out.println();
-            System.out.println();
-        }
+    private static ArrayList<Vacante> crearVacantes1() {
+        ArrayList<Vacante> vacantes = new ArrayList<>();
+        vacantes.add(crearVacante("A", 10));
+        vacantes.add(crearVacante("B", 0));
+        vacantes.add(crearVacante("C", 0));
+        return vacantes;
     }
 
-    private static void imprimirResultadosSinRepetidos(ArrayList<Resultado> resultados) {
-        for (Resultado resultado : resultados) {
-            boolean valid = true;
-            for(int i = 0; i < resultado.puestosCubiertos.size(); i++) {
-                if (i == 0 && !resultado.puestosCubiertos.get(i).nombreVacante.equals("A")) {
-                    valid = false;
-                }
-                if (i == 1 && !resultado.puestosCubiertos.get(i).nombreVacante.equals("B")) {
-                    valid = false;
-                }
-                if (i == 2 && !resultado.puestosCubiertos.get(i).nombreVacante.equals("C")) {
-                    valid = false;
-                }
-            }
-            if (valid) {
+    private static ArrayList<Candidato> crearCandidatos1() {
+        ArrayList<Candidato> candidatos = new ArrayList<>();
+        candidatos.add(crearCandidato(1, 15));
+        candidatos.add(crearCandidato(2, 12));
+        candidatos.add(crearCandidato(3, 9));
+        return candidatos;
+    }
+
+    public static ArrayList<Resultado> ejemplo2() {
+        ResolverCandidatosImplementacion encontrarCandidatos = new ResolverCandidatosImplementacion();
+        ArrayList<Vacante> vacantes = crearVacantes2();
+        ArrayList<Candidato> candidatos = crearCandidatos2();
+        int maxCombinaciones = 4;
+
+        return encontrarCandidatos.obtenerCandidatos(vacantes, candidatos, maxCombinaciones);
+    }
+
+    private static ArrayList<Vacante> crearVacantes2() {
+        ArrayList<Vacante> vacantes = new ArrayList<>();
+        vacantes.add(crearVacante("A", 10));
+        vacantes.add(crearVacante("B", 0));
+        vacantes.add(crearVacante("C", 0));
+        vacantes.add(crearVacante("D", 0));
+        return vacantes;
+    }
+
+    private static ArrayList<Candidato> crearCandidatos2() {
+        ArrayList<Candidato> candidatos = new ArrayList<>();
+        candidatos.add(crearCandidato(1, 9));
+        candidatos.add(crearCandidato(2, 4));
+        candidatos.add(crearCandidato(3, 5));
+        candidatos.add(crearCandidato(4, 8));
+        candidatos.add(crearCandidato(5, 1));
+        return candidatos;
+    }
+
+    private static Vacante crearVacante(String nombreVacante, int califMinima) {
+        Vacante vacante = new Vacante();
+        vacante.nombreVacante = nombreVacante;
+        vacante.califMinima = califMinima;
+        return vacante;
+    }
+
+    private static Candidato crearCandidato(int idCandidato, int calificacion) {
+        Candidato candidato = new Candidato();
+        candidato.idCandidato = idCandidato;
+        candidato.calificacion = calificacion;
+        return candidato;
+    }
+
+    private static void imprimirResultados(String titulo, ArrayList<Resultado> resultados) {
+        System.out.println(titulo);
+        System.out.println();
+
+        if (!resultados.isEmpty()) {
+            for (Resultado resultado : resultados) {
                 System.out.println("Resultado " + resultado.idResultado);
 
                 for (PuestoCubierto puesto : resultado.puestosCubiertos) {
@@ -60,37 +92,9 @@ public class Principal {
                 System.out.println();
                 System.out.println();
             }
+        } else {
+            System.out.println("No hay resultados");
         }
-    }
-
-    private static ArrayList<Vacante> crearVacantes() {
-        ArrayList<Vacante> vacantes = new ArrayList<>();
-        vacantes.add(crearVacante("A", 10));
-        vacantes.add(crearVacante("B", 0));
-        vacantes.add(crearVacante("C", 0));
-        return vacantes;
-    }
-
-    private static Vacante crearVacante(String nombreVacante, int califMinima) {
-        Vacante vacante = new Vacante();
-        vacante.nombreVacante = nombreVacante;
-        vacante.califMinima = califMinima;
-        return vacante;
-    }
-
-    private static ArrayList<Candidato> crearCandidatos() {
-        ArrayList<Candidato> candidatos = new ArrayList<>();
-        candidatos.add(crearCandidato(1, 15));
-        candidatos.add(crearCandidato(2, 12));
-        candidatos.add(crearCandidato(3, 9));
-        return candidatos;
-    }
-
-    private static Candidato crearCandidato(int idCandidato, int calificacion) {
-        Candidato candidato = new Candidato();
-        candidato.idCandidato = idCandidato;
-        candidato.calificacion = calificacion;
-        return candidato;
     }
 }
 
